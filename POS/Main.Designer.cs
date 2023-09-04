fed7add976b1d7ea6ac82a8f4b1db998218ea3a1
zero <a0970937127@gmail.com>
2023-09-04 20:23:07 +0800

    first commit

---
+++ POS/Main.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace POS
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.productlistView = new System.Windows.Forms.ListView();
+            this.productBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // productlistView
+            // 
+            this.productlistView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.productlistView.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.productlistView.HideSelection = false;
+            this.productlistView.Location = new System.Drawing.Point(0, 0);
+            this.productlistView.Name = "productlistView";
+            this.productlistView.Size = new System.Drawing.Size(1022, 678);
+            this.productlistView.TabIndex = 0;
+            this.productlistView.UseCompatibleStateImageBehavior = false;
+            // 
+            // productBtn
+            // 
+            this.productBtn.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.productBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.productBtn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.productBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.productBtn.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.productBtn.Location = new System.Drawing.Point(1325, 0);
+            this.productBtn.Name = "productBtn";
+            this.productBtn.Size = new System.Drawing.Size(101, 678);
+            this.productBtn.TabIndex = 1;
+            this.productBtn.Text = "產品管理";
+            this.productBtn.UseVisualStyleBackColor = true;
+            this.productBtn.Click += new System.EventHandler(this.productBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(1224, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 678);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button3.Location = new System.Drawing.Point(1123, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 678);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button4.Location = new System.Drawing.Point(1022, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 678);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1426, 678);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.productBtn);
+            this.Controls.Add(this.productlistView);
+            this.Name = "Main";
+            this.Text = "POS 機器";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView productlistView;
+        private System.Windows.Forms.Button productBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ POS/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            ImageList il = new ImageList();
+            il.ImageSize = new Size(256, 256);
+            il.Images.Add("test1", Image.FromFile(@"D:\網頁專案\布哥\hanying\hanying-project\hanying-api\public\float\img\philosophy.png"));
+            il.Images.Add("test2", Image.FromFile(@"D:\網頁專案\布哥\hanying\hanying-project\hanying-api\public\float\img\philosophy.png"));
+            il.Images.Add("test3", Image.FromFile(@"D:\網頁專案\布哥\hanying\hanying-project\hanying-api\public\float\img\philosophy.png"));
+
+            productlistView.View = View.LargeIcon;
+            productlistView.LargeImageList = il;
+
+            for (int i = 0; i < il.Images.Count; i++)
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.ImageIndex = i;
+                lvi.Text = "koala 1";
+                productlistView.Items.Add(lvi);
+            }
+        }
+
+        private void productBtn_Click(object sender, EventArgs e)
+        {
+            var productForm = new Products.Main();
+            productForm.ShowDialog();
+        }
+    }
+}
