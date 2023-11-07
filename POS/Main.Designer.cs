dfb756c901661a8c9213eb7dc4e9e8eb9d62d9a6
zero <a0970937127@gmail.com>
2023-11-07 20:24:10 +0800

    update main style.

--- POS/Main.Designer.cs
+++ POS/Main.Designer.cs
@@ -31,9 +31,8 @@ namespace POS
         {
             this.productlistView = new System.Windows.Forms.ListView();
             this.productBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // productlistView
@@ -41,63 +40,56 @@ namespace POS
             this.productlistView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.productlistView.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.productlistView.BackColor = System.Drawing.Color.LightCyan;
             this.productlistView.HideSelection = false;
-            this.productlistView.Location = new System.Drawing.Point(0, 0);
+            this.productlistView.Location = new System.Drawing.Point(3, 0);
             this.productlistView.Name = "productlistView";
-            this.productlistView.Size = new System.Drawing.Size(1022, 678);
+            this.productlistView.Size = new System.Drawing.Size(1422, 558);
             this.productlistView.TabIndex = 0;
             this.productlistView.UseCompatibleStateImageBehavior = false;
             // 
             // productBtn
             // 
             this.productBtn.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.productBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.productBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.productBtn.Dock = System.Windows.Forms.DockStyle.Right;
             this.productBtn.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.productBtn.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.productBtn.Location = new System.Drawing.Point(1325, 0);
+            this.productBtn.Location = new System.Drawing.Point(3, 558);
             this.productBtn.Name = "productBtn";
-            this.productBtn.Size = new System.Drawing.Size(101, 678);
+            this.productBtn.Size = new System.Drawing.Size(258, 122);
             this.productBtn.TabIndex = 1;
             this.productBtn.Text = "產品管理";
             this.productBtn.UseVisualStyleBackColor = true;
             this.productBtn.Click += new System.EventHandler(this.productBtn_Click);
             // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(262, 558);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 122);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "產品管理";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // button2
             // 
             this.button2.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button2.Location = new System.Drawing.Point(1224, 0);
+            this.button2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button2.Location = new System.Drawing.Point(521, 558);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 678);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 122);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "產品管理";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
-            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button3.Location = new System.Drawing.Point(1123, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(101, 678);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
-            this.button4.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button4.Location = new System.Drawing.Point(1022, 0);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(101, 678);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
@@ -105,9 +97,8 @@ namespace POS
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1426, 678);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.productBtn);
             this.Controls.Add(this.productlistView);
             this.Name = "Main";
@@ -121,9 +112,8 @@ namespace POS
 
         private System.Windows.Forms.ListView productlistView;
         private System.Windows.Forms.Button productBtn;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
