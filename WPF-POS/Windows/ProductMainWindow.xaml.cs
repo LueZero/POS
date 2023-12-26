84a2fd9091ebbee189dee763fb3424684b32d7a5
zero <a0970937127@gmail.com>
2023-12-26 21:16:05 +0800

    add product main window.

--- WPF-POS/Windows/ProductMainWindow.xaml.cs
+++ WPF-POS/Windows/ProductMainWindow.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WPF_POS.Windows;
 
-namespace WPF_POS
+namespace WPF_POS.Windows
 {
     /// <summary>
-    /// MainWindow.xaml 的互動邏輯
+    /// ProductMainWindow.xaml 的互動邏輯
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ProductMainWindow : Window
     {
-        public MainWindow()
+        public ProductMainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var productMainWindow = new ProductMainWindow();
-            productMainWindow.Show();
         }
     }
 }
